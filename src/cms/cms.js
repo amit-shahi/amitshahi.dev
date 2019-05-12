cf24cc82efeffd343544077c3d7f9a596e452d4b
Amit Shahi <ami.oneworld@gmail.com>
2019-05-12 22:20:38 +0530

    Removed product page reference
    Removed product page reference

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -9,3 +9,6 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 //CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+
+
