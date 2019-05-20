1ec6e8cdf4e3d641c27a11ef2972a005af85c148
Amit Shahi <ami.oneworld@gmail.com>
2019-05-21 03:00:48 +0530

    removed GA js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -47,15 +47,6 @@ const TemplateWrapper = ({ children }) => {
       <Navbar />
       <div>{children}</div>
       <Footer />
-{/* <script async src="https://www.googletagmanager.com/gtag/js?id=UA-61172932-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments)
-    }
-  gtag('js', new Date());
-  gtag('config', 'UA-61172932-2');
-</script> */}
     </div>
   )
 }
